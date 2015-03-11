df208fc162463dff044ca8aa9f5a894e732a4b4d
Attila M. Magyar <athos@balabit.com>
2015-03-11 21:08:27 +0100

    features: introduce behave for further development of features

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ PYTHON ?= python
 NOSE = $(PYTHON) -m nose
 PYLINT = $(PYTHON) -m pylint
 PEP8 = $(PYTHON) -m pep8
+BEHAVE = $(PYTHON) -m behave
 
 TESTDIRS = typesafety/
 
@@ -12,8 +13,15 @@ pre-commit: codingstandards check
 pre-merge: pre-commit documentation
 
 .PHONY: check
-check:
+check: tests features
+
+.PHONY: tests
+tests:
 	$(NOSE) --with-coverage --with-doctest -s $(TESTDIRS)
+
+.PHONY: features
+features:
+	$(BEHAVE) features --format progress2
 
 .PHONY: codingstandards
 codingstandards: check-copyright
---
+++ features/steps/steps.py
@@ -0,0 +1,58 @@
+#
+# Copyright (c) 2013-2015 BalaBit
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+import sys
+import os.path
+import shlex
+import subprocess
+
+from nose.tools import *
+from behave import given, when, then
+
+
+assert_equal.__self__.maxDiff = None
+
+
+@given('that "{file_name}" contains the following code')
+def write_file(context, file_name):
+    with open(os.path.join(context.test_dir, file_name), "w") as f:
+        f.write(context.text)
+
+
+@when('"{command}" is run')
+def execute_command(context, command: str):
+    args = shlex.split(command)
+    process = subprocess.Popen(
+        args,
+        stdin=None,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        env={"PYTHONPATH": "{}:{}".format(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), sys.path)}
+    )
+    output = process.communicate()[0].decode()
+    return_code = process.returncode
+
+    context.commands.append((command, return_code, output))
+
+
+@then('it must fail')
+def assert_failure(context):
+    expected_output = context.text
+    command, return_code, output = context.commands[-1]
+
+    assert_in(expected_output, output, "Unexpected output of {!r}".format(command))
+    assert_not_equal(0, return_code, "Unexpected return code of {!r}".format(command))
